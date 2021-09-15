fca6ca6437a1bca4d12ed8f9254d9729b760ca8d
duanlihua <13787599570@163.com>
2021-09-15 15:24:12 +0800

    antd配置主题色

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,17 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader} = require('customize-cra');
+const { getThemeVariables } = require('antd/dist/theme');
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {'@primary-color': '#1DA57A'}, // 更换主题色
+        // modifyVars: getThemeVariables({
+        //     dark: true, // 开启暗黑模式
+        //     compact: true, // 开启紧凑模式
+        // }),
     }),
 );
